3d8e1fba46f164c047f1622c2e6318bc12eb832a
Teviets <est21405@uvg.edu.gt>
2022-10-19 17:06:57 -0600

    Arreglos de vistas

---
+++ BUVGM/app/src/main/java/com/buvgm/ui/InternalActivity.kt
@@ -0,0 +1,11 @@
+package com.buvgm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InternalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_internal)
+    }
+}
