54732405fc8f7cc9f768de1c2a3db6f0e59d9741
Teviets <est21405@uvg.edu.gt>
2022-11-21 14:35:30 -0600

    Binding y view model de new product

--- BUVGM/app/src/main/java/com/buvgm/ui/nuevoproducto/NewProductFragment.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/nuevoproducto/NewProductFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.buvgm.R
+import com.buvgm.databinding.FragmentNewProductBinding
 
 class NewProductFragment : Fragment(R.layout.fragment_new_product) {
+    private lateinit var binding: FragmentNewProductBinding
+    private val NewProductViewModel: NewProductViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_product, container, false)
+        binding = FragmentNewProductBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
