a9c9f3f7b58d919ef2555066f3ad5f3feaa02a13
Teviets <est21405@uvg.edu.gt>
2022-11-21 14:20:46 -0600

    ViewModels

--- BUVGM/app/src/main/java/com/buvgm/ui/login/LoginViewModel.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/login/LoginViewModel.kt
@@ -1,6 +1,39 @@
 package com.buvgm.ui.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
+    private val _statusApp: MutableStateFlow<StatusApp> = MutableStateFlow<StatusApp>(StatusApp.default)
+    val statusApp : StateFlow<StatusApp> = _statusApp
+
+    sealed class  StatusApp{
+        object default: StatusApp()
+        object succes: StatusApp()
+        object failure: StatusApp()
+        object loading: StatusApp()
+    }
+
+    fun reset(){
+        _statusApp.value = StatusApp.default
+    }
+
+    fun exito(){
+        _statusApp.value = StatusApp.succes
+    }
+
+    fun fallo(){
+        _statusApp.value = StatusApp.failure
+    }
+
+    fun carga(){
+        viewModelScope.launch {
+            _statusApp.value = StatusApp.loading
+            delay(1000L)
+        }
+    }
 }
--- BUVGM/app/src/main/java/com/buvgm/ui/perfilproducto/ProductProfileViewModel.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/perfilproducto/ProductProfileViewModel.kt
@@ -1,6 +1,34 @@
 package com.buvgm.ui.perfilproducto
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ProductProfileViewModel: ViewModel() {
+    private val _statusApp: MutableStateFlow<StatusApp> = MutableStateFlow<StatusApp>(StatusApp.default)
+    val statusApp : StateFlow<StatusApp> = _statusApp
+
+    sealed class  StatusApp{
+        object default: StatusApp()
+        object failure: StatusApp()
+        object loading: StatusApp()
+    }
+
+    fun reset(){
+        _statusApp.value = StatusApp.default
+    }
+
+    fun fallo(){
+        _statusApp.value = StatusApp.failure
+    }
+
+    fun carga(){
+        viewModelScope.launch {
+            _statusApp.value = StatusApp.loading
+            delay(1000L)
+        }
+    }
 }
