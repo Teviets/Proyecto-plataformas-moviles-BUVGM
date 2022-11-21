d1b75e532b3ca2b255cd8dbc242a470db801d81d
Teviets <est21405@uvg.edu.gt>
2022-11-21 16:50:45 -0600

    Backend de new Account
    Solo falta verificacion de usuario

--- BUVGM/app/src/main/java/com/buvgm/ui/registro/NewAccountFragment.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/registro/NewAccountFragment.kt
@@ -1,19 +1,38 @@
 package com.buvgm.ui.registro
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.buvgm.R
 import com.buvgm.databinding.FragmentNewAccountBinding
+import com.buvgm.ui.InternalActivity
+import kotlinx.coroutines.flow.collectLatest
+import java.io.File
+import java.util.jar.Manifest
 
 class NewAccountFragment : Fragment() {
     private val NewAccountViewModel: NewAccountViewModel by viewModels()
 
     private lateinit var binding: FragmentNewAccountBinding
-
+    private val SELECT_ACTIVITY = 1
+    private var imageUri : Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +41,58 @@ class NewAccountFragment : Fragment() {
         binding = FragmentNewAccountBinding.inflate(inflater,container,false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+        setObservers()
+    }
+
+    private fun setListeners() {
+        binding.RegisterButton.setOnClickListener {
+            verificacion()
+        }
+    }
+
+    private fun verificacion() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setObservers() {
+        lifecycleScope.launchWhenCreated {
+            NewAccountViewModel.statusApp.collectLatest {
+                verStatus(it)
+            }
+        }
+    }
+
+    private fun verStatus(it: NewAccountViewModel.StatusApp) {
+        when(it){
+            is NewAccountViewModel.StatusApp.default ->{
+                binding.RegisterButton.visibility = View.VISIBLE
+                binding.progressBarNewAccount.visibility = View.GONE
+            }
+            is NewAccountViewModel.StatusApp.loading ->{
+                binding.RegisterButton.visibility = View.GONE
+                binding.progressBarNewAccount.visibility = View.VISIBLE
+            }
+            is NewAccountViewModel.StatusApp.failure ->{
+                binding.RegisterButton.visibility = View.VISIBLE
+                binding.progressBarNewAccount.visibility = View.GONE
+                binding.errImg.load(R.drawable.ic_error)
+                binding.errImg.visibility = View.VISIBLE
+            }
+            is NewAccountViewModel.StatusApp.succes ->{
+                binding.errImg.load(R.drawable.usericon)
+                binding.errImg.visibility = View.VISIBLE
+            }
+    }
+}
+
+
+
+
+
+
 
 }
