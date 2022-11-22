6056063326ea32c953455472b94dbee0a089c0f9
Teviets <est21405@uvg.edu.gt>
2022-11-21 18:26:10 -0600

    Correccion de funcionamiento
    Correccion de view model

--- BUVGM/app/src/main/java/com/buvgm/ui/InternalActivity.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/InternalActivity.kt
@@ -2,24 +2,38 @@ package com.buvgm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.webkit.RenderProcessGoneDetail
 import android.widget.Toolbar
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import com.buvgm.R
 import com.buvgm.ui.busqueda.SearchFragment
+import com.buvgm.ui.busqueda.SearchFragmentDirections
+import com.buvgm.ui.configuracion.SettingsFragment
 import com.buvgm.ui.favoritos.FavoritesFragment
+import com.buvgm.ui.favoritos.FavoritesFragmentDirections
+import com.buvgm.ui.login.LoginFragmentDirections
 import com.buvgm.ui.nuevoproducto.NewProductFragment
 import com.buvgm.ui.productos.ProductsHomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.flow.collectLatest
 
 class InternalActivity : AppCompatActivity() {
 
     private lateinit var bottomNav: BottomNavigationView
     private lateinit var tool: androidx.appcompat.widget.Toolbar
     private lateinit var container: FragmentContainerView
+
+    private var estado: String = ""
+    private val InternalViewModel: InternalViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_internal)
@@ -28,8 +42,10 @@ class InternalActivity : AppCompatActivity() {
         tool = findViewById(R.id.principal_toolbar)
         container = findViewById(R.id.principal_fragmentContainerView)
 
-        //setListeners()
+        setListeners()
+        
     }
+
 
     private fun setListeners() {
         bottomNav.setOnItemSelectedListener {
@@ -45,15 +61,15 @@ class InternalActivity : AppCompatActivity() {
         tool.setOnMenuItemClickListener {
             when(it.itemId){
                 R.id.nuevoProducto_item -> {
-                    tool.visibility = GONE
-                    bottomNav.visibility = GONE
+                    tool.visibility = View.GONE
+                    bottomNav.visibility = View.GONE
                     setFragment(NewProductFragment())
                     true
                 }
                 R.id.settings_item -> {
                     tool.visibility = GONE
                     bottomNav.visibility = GONE
-                    setFragment(NewProductFragment())
+                    setFragment(SettingsFragment())
                     true
                 }
                 else -> true
@@ -64,8 +80,15 @@ class InternalActivity : AppCompatActivity() {
     private fun setFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            replace(R.id.principal_fragmentContainerView, fragment)
+            replace(R.id.principal_fragmentContainerView, fragment).addToBackStack(null)
         }
     }
+
+    override fun onBackPressed() {
+        tool.visibility = VISIBLE
+        bottomNav.visibility = View.VISIBLE
+        super.onBackPressed()
+    }
+
 
 }
