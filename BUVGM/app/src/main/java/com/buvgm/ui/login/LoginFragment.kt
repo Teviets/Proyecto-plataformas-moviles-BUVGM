4d94f6d448cb1e9881b2474fa9ddd24f87a9a890
Teviets <est21405@uvg.edu.gt>
2022-11-22 01:13:27 -0600

    Primera union data ui

--- BUVGM/app/src/main/java/com/buvgm/ui/login/LoginFragment.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/login/LoginFragment.kt
@@ -7,22 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
-import androidx.navigation.Navigation
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
-import com.buvgm.R
 import com.buvgm.databinding.FragmentLoginBinding
 import com.buvgm.ui.InternalActivity
-import com.buvgm.ui.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private val LoginViewModel:LoginViewModel by viewModels()
+    @Inject
+    private lateinit var authRepository: AuthRepository
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,12 +52,10 @@ class LoginFragment : Fragment() {
             CoroutineScope(Dispatchers.Main).launch {
                 delay(1000L)
             }
-            /*verificacion(
-                email = binding.inputLayoutLoginFragmentEmail.editText!!.text.toString(),
-                password = binding.inputLayoutLoginFragmentPassword.editText!!.text.toString(),
-                //Mutable list de emails,
-                // Mutable list de password
-            )*/
+        verificacion(
+            email = binding.inputLayoutLoginFragmentEmail.editText!!.text.toString(),
+            password = binding.inputLayoutLoginFragmentPassword.editText!!.text.toString()
+            )
         }
         binding.clicklabeTextViewRegister.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToNewAccountFragment()
@@ -61,20 +63,40 @@ class LoginFragment : Fragment() {
         }
     }
 
-    private fun verificacion(email: String,password: String, emails: MutableList<String>, passwords: MutableList<String>){
+    private fun verificacion(email: String,password: String){
         binding.loginButton.visibility = View.GONE
         binding.ProgressbarLogin.visibility = View.VISIBLE
-        if ((emails.contains(email) == true) && (passwords.contains(password) == true)){
-            val intent = Intent(context, InternalActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(intent)
-        }else{
-            Toast.makeText(requireContext(),getString(R.string.err_login),Toast.LENGTH_LONG).show()
-            CoroutineScope(Dispatchers.Main).launch {
-                delay(1000L)
+        binding.loginButton.setOnClickListener {
+            val email = binding.inputLayoutLoginFragmentEmail.editText!!.text.toString()
+            val password = binding.inputLayoutLoginFragmentPassword.editText!!.text.toString()
+
+
+            lifecycleScope.launch {
+                val userId = authRepository.signInWithEmailAndPassword(email, password)
+                if (!userId.isNullOrEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Usuario loggeado exitosamente",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Intent(requireContext(), InternalActivity::class.java).apply {  //Mover hacia donde se requiere siguiente fragment
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        putExtras(bundleOf("id" to userId))
+                        startActivity(this)
+                    }
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Usuario o contraseña incorrectos",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    CoroutineScope(Dispatchers.Main).launch {
+                        delay(1000L)
+                    }
+                    binding.loginButton.visibility = View.VISIBLE
+                    binding.ProgressbarLogin.visibility = View.GONE
+                }
             }
-            binding.loginButton.visibility = View.VISIBLE
-            binding.ProgressbarLogin.visibility = View.GONE
         }
 
     }
