411e3e3aabb5a91995240dc2dac4dcac4db76853
ManuelR11 <84192643+ManuelR11@users.noreply.github.com>
2022-11-21 19:27:09 -0600

    Login activo

--- BUVGM/app/src/main/java/com/buvgm/ui/login/LoginFragment.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/login/LoginFragment.kt
@@ -6,42 +6,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.buvgm.R
+import com.buvgm.data.model.remote.firebase.FirebaseAuthApiImpl
 import com.buvgm.data.model.repository.auth.AuthRepository
+import com.buvgm.data.model.repository.auth.AuthRepositoryImpl
+import com.buvgm.databinding.ActivityMainBinding
 import com.buvgm.databinding.FragmentLoginBinding
 import com.buvgm.ui.InternalActivity
 import com.buvgm.ui.MainActivity
+import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginFragment : Fragment() {
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
-    private lateinit var binding: FragmentLoginBinding
+    private lateinit var buttonLogin: Button
+    private lateinit var buttonNewAccount: Button
+    private lateinit var inputEmail: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+
+    @Inject
     private lateinit var authRepository: AuthRepository
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentLoginBinding.inflate(inflater,container,false )
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onCreateview(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        buttonLogin = view.findViewById(R.id.login_button)
+        buttonNewAccount = view.findViewById(R.id.clicklabe_textView_register)
+        inputEmail = view.findViewById(R.id.inputLayout_loginFragment_email)
+        inputPassword = view.findViewById(R.id.inputLayout_loginFragment_password)
+
         setListeners()
+        //authRepository = AuthRepositoryImpl{
+        //   = FirebaseAuthApiImpl()
+        //}
     }
 
-    private fun setListeners(){
-        binding.loginButton.setOnClickListener{
-            val intent = Intent(context, InternalActivity::class.java)
-            startActivity(intent)
-        }
-        binding.clicklabeTextViewRegister.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToNewAccountFragment()
-            requireView().findNavController().navigate(action)
-            //Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_newAccountFragment)
+
+    private fun setListeners() {
+        buttonLogin.setOnClickListener {
+            val email = inputEmail.editText!!.text.toString()
+            val password = inputPassword.editText!!.text.toString()
+
+
+            lifecycleScope.launch {
+                val userId = authRepository.signInWithEmailAndPassword(email, password)
+                if (!userId.isNullOrEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Usuario loggeado exitosamente",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Intent(requireContext(), InternalActivity::class.java).apply {  //Mover hacia donde se requiere siguiente fragment
+                        putExtras(bundleOf("id" to userId))
+                        startActivity(this)
+                    }
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Usuario o contraseña incorrectos",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
         }
     }
 }
