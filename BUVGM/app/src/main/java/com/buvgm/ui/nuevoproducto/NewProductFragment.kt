fbd76f29ac26ba30a6b794f24aa6b54356ae6651
ManuelR11 <84192643+ManuelR11@users.noreply.github.com>
2022-11-21 23:56:11 -0600

    Creacion New_Product

--- BUVGM/app/src/main/java/com/buvgm/ui/nuevoproducto/NewProductFragment.kt
+++ BUVGM/app/src/main/java/com/buvgm/ui/nuevoproducto/NewProductFragment.kt
@@ -6,8 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import com.buvgm.R
+import com.buvgm.data.model.local.entity.Place
+import com.buvgm.data.model.remote.firebase.FirebasePlaceApiImpl
+import com.buvgm.data.model.repository.place.PlaceRepository
+import com.buvgm.data.model.repository.place.PlaceRepositoryImpl
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NewProductFragment : Fragment(R.layout.fragment_new_product) {
 
@@ -17,13 +26,48 @@ class NewProductFragment : Fragment(R.layout.fragment_new_product) {
     private lateinit var inputPrecio: TextInputLayout
     private lateinit var inputContacto: TextInputLayout
 
+    private lateinit var repository: PlaceRepository
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_new_product, container, false)
-        
+
         buttonUpload = view.findViewById(R.id.button_Upload)
+        inputNombre = view.findViewById(R.id.inputLayout_new_productFragment_nombre)
+        inputDescripcion = view.findViewById(R.id.inputLayout_new_productFragment_descripcion)
+        inputPrecio = view.findViewById(R.id.inputLayout_new_productFragment_precio)
+        inputContacto = view.findViewById(R.id.inputLayout_new_productFragment_contacto)
+
+        repository = PlaceRepositoryImpl(
+            FirebasePlaceApiImpl(Firebase.firestore)
+        )
+
+        initListeners()
+
+    }
+
+    private fun initListeners() {
+        buttonUpload.setOnClickListener {
+            val Nombre = inputNombre.editText!!.text.toString()
+            val Descripcion = inputDescripcion.editText!!.text.toString()
+            val Precio = inputPrecio.editText!!.text.toString()
+            val Contacto = inputContacto.editText!!.text.toString()
+
+            lifecycleScope.launch(Dispatchers.IO){
+                repository.createPlace(
+                    place = Place(
+                        Contacto = Contacto,
+                        Descripcion = Descripcion,
+                        Nombre = Nombre,
+                        Precio = Precio
+                    )
+                )
+
+            }
+
+        }
     }
 
 }
